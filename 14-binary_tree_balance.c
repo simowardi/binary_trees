e76b17c09c8bbbbedeffec4b24c493b992b50de9
simowardi <daoist32@gmail@gmail.com>
2024-03-26 15:06:27 -0700

    update 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -26,7 +26,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 		lef = tree->left ? 1 + binary_tree_height(tree->left) : 1;
 		rig = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-		return ((lef > r) ? lef : rig);
+		return ((lef > rig) ? lef : rig);
 	}
 	return (0);
 }
