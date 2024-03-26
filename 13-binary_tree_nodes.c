6e8360398f6a9ae4a818f281a3fe3f5a01b64190
simowardi <daoist32@gmail@gmail.com>
2024-03-26 15:05:20 -0700

    updat 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,13 +7,13 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t nods = 0;
+	size_t nodes = 0;
 
 	if (tree)
 	{
-		nods += (tree->left || tree->right) ? 1 : 0;
-		nods += binary_tree_nodes(tree->left);
-		nods += binary_tree_nodes(tree->right);
+		nodes += (tree->left || tree->right) ? 1 : 0;
+		nodes += binary_tree_nodes(tree->left);
+		nodes += binary_tree_nodes(tree->right);
 	}
 	return (nodes);
 }
